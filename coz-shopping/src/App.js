cce4c6ee91a37124a76deb4ba9db28d3774ab8f0
shyesoo <shyesoo723@gmail.com>
2023-05-16 02:44:06 +0900

    Feat: Footer 기능 구현

--- coz-shopping/src/App.js
+++ coz-shopping/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Footer';
 import Header from './Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header>
         
       </Header>
+      <Footer />
     </div>
   );
 }
